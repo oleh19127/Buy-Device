ad9b97be12a057fcf0628fda70198782ffec41b8
Oleh Husak <40184919+oleh312@users.noreply.github.com>
2023-06-05 17:37:52 +0300

    register fastifyUpload, fastifyStatic

--- Buy-Device-Backend/app.js
+++ Buy-Device-Backend/app.js
@@ -1,11 +1,13 @@
-import path from "path";
+import { dirname, join } from "path";
 import AutoLoad from "@fastify/autoload";
 import { fileURLToPath } from "url";
 import { sequelize } from "./db/index.js";
 import "./db/models.js";
+import fastifyUpload from "fastify-file-upload";
+import fastifyStatic from "@fastify/static";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 // Pass --options via CLI arguments in command to enable these options.
 export const options = {};
@@ -16,20 +18,26 @@ export default async function (fastify, opts) {
   await sequelize.authenticate();
   await sequelize.sync();
 
-  // Do not touch the following lines
+  // File Upload
+  fastify.register(fastifyUpload);
+
+  // Static files
+  fastify.register(fastifyStatic, {
+    root: join(__dirname, "static"),
+  });
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "plugins"),
+    dir: join(__dirname, "plugins"),
     options: Object.assign({}, opts),
   });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, "routes"),
+    dir: join(__dirname, "routes"),
     options: Object.assign({}, opts),
   });
 }
